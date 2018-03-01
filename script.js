b5d89f1521c90e53ed0a47d968c4e385edf77e84
Autif Kamal <kautif@gmail.com>
2018-02-28 18:58:18 -0600

    JSON data accessed

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello');
+$('#search-field').on('submit', function(e){
+	e.preventDefault();
+});
+
+function handleSearch(data){
+	let output = data.results;
+	console.log(output);
+}
+
+function initialSearch(){
+	let queryStr = $('#query').val();
+
+	$.get(
+		'https://api.themoviedb.org/3/search/tv', 
+		{
+			api_key: '08eba60ea81f9e9cf342c7fa3df07bb6',
+			query: queryStr
+		},
+		handleSearch
+		)
+}
