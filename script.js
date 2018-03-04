5bc1e1abc1e7a55793dfbd718b32c963189ef075
Autif Kamal <kautif@gmail.com>
2018-03-04 16:31:40 -0600

    Completed recommendation request and started lightbox

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ $('#search-field').on('submit', function(e){
 
 function handleSearch(data){
 	let output = data.results;
-	console.log(output);
+	// console.log(output);
 	$('.results-container').html('');
 	let getURLs = [];
 	for (let i = 0; i < output.length; i++) {
 		$('.results-container').append('<div class="result-item"' + 'name="' + output[i].name + '"' + '>' + output[i].name + "<br>" + 
 			// '<a ' + 'href="https://api.themoviedb.org/3/tv/' + output[i].id + '/recommendations?api_key=08eba60ea81f9e9cf342c7fa3df07bb6&language=en-US">' + 
-			'<img src="https://image.tmdb.org/t/p/w200_and_h300_bestv2/' + output[i].poster_path + '"></a>' + '<span class="hideID">' + output[i].id + '</span></div>');
+			'<img id="' + output[i].id + '" src="https://image.tmdb.org/t/p/w200_and_h300_bestv2/' + output[i].poster_path + '">' + '</div>');
 		
 	// if the image equals null, do not display it. 
 	// if the title does not have the search query in the name of the result, do not display it.
@@ -20,37 +20,38 @@ function handleSearch(data){
 		// Need to have a way to access total results before adding a link in order to not display image 
 		// that don't have recommendations
 	}
+	$('.result-item img').click(recommendations);
 	// console.log(getURLs);
 }
 
-function getRec(data){
+function handleRec(data){
 	let rec_output = data.results;
-	// console.log(rec_output);
-	let searchArr = [];
-	$('.result-item').each(function(element){
-		let resultName = $(this).attr('name');
-		searchArr.push(resultName);
-	});
-	// console.log(searchArr);
-
-	$('.result-item img').click(function(){
+	console.log(rec_output);
 		$('.results-container').html('');
 // PLAN: When a user clicks on a link, return the recommendations of what they clicked on.
+		for (let j = 0; j < rec_output.length; j++) {
+			$('.results-container').append('<div class="rec-item">' + rec_output[j].name + "<br>" + 
+				'<img src="https://image.tmdb.org/t/p/w200_and_h300_bestv2/' + rec_output[j].poster_path + '"></div>'); 
+		}	
 
-		for (let j = 0; j < searchArr.length; j++) {
-			$('.results-container').append(rec_output[j].name)
-		}
-	});
-	
+		$('.rec-item').click(function(){
+			$('.results-container').append('<div class="rec-details">f</div>');
+			alert('rec-item');
+		})
+
+		$('.container').click(function(e){
+			// $('.rec-details').remove();
+			// alert('container');
+		})
 }
 
-function recommendations(){
+function recommendations(e){
 	$.get(
-		'https://api.themoviedb.org/3/tv/4303/recommendations',
+		'https://api.themoviedb.org/3/tv/'+ parseInt(e.currentTarget.id) + '/recommendations',
 		{
 			api_key: '08eba60ea81f9e9cf342c7fa3df07bb6',
 		},
-		getRec
+		handleRec
 		)
 }
 
@@ -65,7 +66,7 @@ function initialSearch(){
 		},
 		function getRequests(data){
 			handleSearch(data);
-			recommendations(data);	
+			// recommendations(data);
 		}
 		
 		)
